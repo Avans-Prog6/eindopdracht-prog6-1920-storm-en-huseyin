e53b0624896cf5717851c945df8d46bdf240515b
Storm <stormwitziers123@gmail.com>
2020-01-06 18:52:51 +0100

    Added basic animal class
    Added
    - The Animal class
    - The AnimalTypes class

---
+++ BeestjeOpJeFeestje/BeestjeOpJeFeestje/Models/Animal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeestjeOpJeFeestje.Models
+{
+    public class Animal
+    { 
+        public string Name { get; private set; }
+        public string Type { get; private set; }
+        public double Price { get; private set; }
+        public string PicturePath { get; private set; }
+
+        public Animal(string name, double price, string picturePath, string type = AnimalTypes.Boerderij)
+        {
+            Type = type;
+            Name = name;
+            Price = price;
+            PicturePath = picturePath;
+        }
+    }
+}
---
+++ BeestjeOpJeFeestje/BeestjeOpJeFeestje/Models/AnimalTypes.cs
@@ -0,0 +1,10 @@
+﻿namespace BeestjeOpJeFeestje.Models
+{
+    public static class AnimalTypes
+    {
+        public const string Woestijn = "Woestijn";
+        public const string Jungle = "Jungle";
+        public const string Sneeuw = "Sneeuw";
+        public const string Boerderij = "Boerderij";
+    }
+}
