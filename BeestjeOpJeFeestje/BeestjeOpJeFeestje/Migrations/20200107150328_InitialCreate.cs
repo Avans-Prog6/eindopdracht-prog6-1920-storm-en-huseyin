94dcae19fd404faee0324af0dc78cdf42349f622
Huseyin Caliskan <huseyincaliskan32@gmail.com>
2020-01-07 16:04:47 +0100

    Database has many to many relationship

---
+++ BeestjeOpJeFeestje/BeestjeOpJeFeestje/Migrations/20200107150328_InitialCreate.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BeestjeOpJeFeestje.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Accessorieses",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Price = table.Column<double>(nullable: false),
+                    PicturePath = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accessorieses", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Animal",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Type = table.Column<string>(nullable: true),
+                    Price = table.Column<double>(nullable: false),
+                    PicturePath = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Animal", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AnimalAccessories",
+                columns: table => new
+                {
+                    AnimalId = table.Column<int>(nullable: false),
+                    AccessoriesId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AnimalAccessories", x => new { x.AnimalId, x.AccessoriesId });
+                    table.ForeignKey(
+                        name: "FK_AnimalAccessories_Accessorieses_AccessoriesId",
+                        column: x => x.AccessoriesId,
+                        principalTable: "Accessorieses",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AnimalAccessories_Animal_AnimalId",
+                        column: x => x.AnimalId,
+                        principalTable: "Animal",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AnimalAccessories_AccessoriesId",
+                table: "AnimalAccessories",
+                column: "AccessoriesId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AnimalAccessories");
+
+            migrationBuilder.DropTable(
+                name: "Accessorieses");
+
+            migrationBuilder.DropTable(
+                name: "Animal");
+        }
+    }
+}
