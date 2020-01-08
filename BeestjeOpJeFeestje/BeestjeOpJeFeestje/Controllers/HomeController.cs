197e272077ce60693b4fef8caf84ad3f4aa974a7
Huseyin Caliskan <huseyincaliskan32@gmail.com>
2020-01-08 22:24:13 +0100

    addead header for accessories

--- BeestjeOpJeFeestje/BeestjeOpJeFeestje/Controllers/HomeController.cs
+++ BeestjeOpJeFeestje/BeestjeOpJeFeestje/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace BeestjeOpJeFeestje.Controllers
             return View();
         }
 
+        public IActionResult Accessories()
+        {
+	        return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
